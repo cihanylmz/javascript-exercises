1ab3ca7e7f6b176c1e3b34ee4532231969d7f58b
Cihan <ylmz_cihan@outlook.com>
2022-02-26 10:59:27 +0300

    Solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(opOne, opTwo) {
+    if (opOne < 0 || opTwo < 0) return "ERROR";
+    
+    //If not a number, throw ERROR
+    if (typeof opOne != 'number' || typeof opTwo != 'number' ) return "ERROR";
 
+    let sum = 0;
+    let numCount = Math.abs(opTwo - opOne) + 1;
+    for (let i = 0; i < numCount; i++) {
+        //Subtract the smaller one from the bigger one.
+        sum += (opOne > opTwo ? opTwo : opOne) + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
