d510bbf05a4e2be4f36e85c04646b7aab8b8348c
Cihan <ylmz_cihan@outlook.com>
2022-03-07 01:22:25 +0300

    Solved exercises 10 through 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const byLifeTime = people.sort((personA, personB) => {
+    const date = (new Date()).getFullYear();
+    return findAge(personA) - findAge(personB);
+  });
+  
+  return byLifeTime[byLifeTime.length - 1];
 };
+
+const findAge = function(person) {
+  return (person.hasOwnProperty('yearOfDeath') ? person.yearOfDeath - person.yearOfBirth : date - person.yearOfBirth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
